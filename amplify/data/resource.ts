79c0ff3de5f707a9de3850def3e1d01973a25857
Oleg Karpov <mail@okarpov.com>
2026-02-05 10:18:18 +1100

    Update data auth / API key expiry

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -23,5 +23,8 @@ export const data = defineData({
   schema,
   authorizationModes: {
     defaultAuthorizationMode: 'apiKey',
+    apiKeyAuthorizationMode: {
+      expiresInDays: 365,
+    },
   }
 });
